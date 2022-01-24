9e882d8526368c6b93b8f61bbb803ae8520959f1
Braeden Cullen <33903031+BraedenCu@users.noreply.github.com>
2022-01-24 17:14:03 -0600

    level one done

---
+++ usingvectors.cs
@@ -0,0 +1,16 @@
+﻿using Utility;
+
+public class Class1
+{
+	public Class1()
+	{
+		var vector = new Vector(6, 4, 3.2);
+		++vector.X;
+		var vector2 = new Vector(2, 4, 8);
+		Console.WriteLine(vector2.X);
+		Console.WriteLine(vector.add(vector, vector2));
+		// 7 * v2 - v1
+		Console.WriteLine(vector.Add(vector.Scale(vector, -1), vector.Scale(vector2, 7)));
+		Console.WriteLine(7 * vector2 - vector);
+	}
+}
